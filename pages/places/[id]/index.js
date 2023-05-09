10adfa2d6e7bed377321af0c6fa4c62a48e3fc4e
Ekaterina Chernova <chernova.berlin@gmail.com>
2023-05-09 11:50:18 +0200

    Styling

--- pages/places/[id]/index.js
+++ pages/places/[id]/index.js
@@ -1,10 +1,10 @@
-import Link from 'next/link';
-import { useRouter } from 'next/router.js';
-import useSWR from 'swr';
-import styled from 'styled-components';
-import { StyledLink } from '../../../components/StyledLink.js';
-import { StyledButton } from '../../../components/StyledButton.js';
-import { StyledImage } from '../../../components/StyledImage.js';
+import Link from "next/link";
+import { useRouter } from "next/router.js";
+import useSWR from "swr";
+import styled from "styled-components";
+import { StyledLink } from "../../../components/StyledLink.js";
+import { StyledButton } from "../../../components/StyledButton.js";
+import { StyledImage } from "../../../components/StyledImage.js";
 
 const ImageContainer = styled.div`
   position: relative;
@@ -32,18 +32,17 @@ export default function DetailsPage() {
   const router = useRouter();
   const { isReady } = router;
   const { id } = router.query;
-
   const { data: place, isLoading, error } = useSWR(`/api/places/${id}`);
-
+  console.log("id", id);
   if (!isReady || isLoading || error) return <h2>Loading...</h2>;
 
   function deletePlace() {
-    console.log('deleted?');
+    console.log("deleted?");
   }
 
   return (
     <>
-      <Link href={'/'} passHref legacyBehavior>
+      <Link href={"/"} passHref legacyBehavior>
         <StyledLink justifySelf="start">back</StyledLink>
       </Link>
       <ImageContainer>
