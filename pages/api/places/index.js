ffd1dc87b39c67c42f675169741448866589e597
Ekaterina Chernova <chernova.berlin@gmail.com>
2023-05-09 10:51:15 +0200

    implement GET method

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,5 +1,16 @@
-import { places } from '../../../lib/db';
+import dbConnect from "db/connect";
+import Location from "db/models/Location";
 
-export default function handler(request, response) {
-  return response.status(200).json(places);
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "GET") {
+    const locations = await Location.find();
+    return response.status(200).json(locations);
+  }
 }
+
+// import { places } from '../../../lib/db';
+// export default function handler(request, response) {
+//   return response.status(200).json(places);
+// }
